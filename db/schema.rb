c36d3c1ab3c30b432d0bdfdf4808ae865e66bc4a
shehrozirfan <shehrozirfan89@gmail.com>
2022-09-25 00:37:30 +0500

    Reverting a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_140558) do
+ActiveRecord::Schema.define(version: 2022_09_24_140731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2022_09_24_140558) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "full_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "full_name"
   end
 
   add_foreign_key "products", "styles"
