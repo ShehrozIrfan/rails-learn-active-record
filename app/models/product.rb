d42dcba7747e22bf5fb3bbd50914752e0fe4037f
Shehroz Irfan <>
2022-09-25 19:56:55 +0500

    Default and named scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  #Here I've defined the default scope that will always be applied on the product
+  #If you don't want to apply default scope use Product.unscoped
+  default_scope { order(created_at: :desc) }
+
+  #Below is the named scope that will be called like: Product.recent
+  scope :recent, lambda { where("created_at >= ?", 1.day.ago) }
 end
