36e561b950e5c4f1f953cb55117e8abcc32ab35e
Shehroz Irfan <>
2022-09-26 12:09:16 +0500

    Delegate added when the attribute is not in the database, we can define it in  model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :products
+
+  def prefix
+    "GEN"
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,13 @@ class Product < ApplicationRecord
 
   #Below is the named scope that will be called like: Product.recent
   scope :recent, lambda { where("created_at >= ?", 1.day.ago) }
+
+  #Delegate allow a request for an object’s attribute to be delegated or referred to another object.
+  #The delegate works in a way that the child ask it's parent for the attribute.
+  delegate :prefix, to: :category
+
+  #The above delegate code is same as:
+  # def prefix
+  #  category.prefix
+  # end
 end
