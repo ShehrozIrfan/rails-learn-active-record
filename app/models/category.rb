6fe2d2a32094adf455a8f3c4aea2a2166ab8f402
Shehroz Irfan <>
2022-09-27 11:59:16 +0500

    Use queries with associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,20 @@
 class Category < ApplicationRecord
-  has_many :products
+  # adding the query to association - remember it will only be called when there is a call to category.products, it will not called when the model is loaded
+  # as we've default scope present in product.rb
+  # which will apply the order to DESC
+  # we can undo that by:
+  # 1. lambda { unscope(:order).order(:created_at) } OR
+  # 2. lamda { reorder(:created_at) } - it will reorder it
+
+  # has_many :products, lambda { reorder(:created_at) }
+
+  # we can also add where and other queries in association like:
+  has_many :products, lambda { where("qty_sold > 1") }
+
+  # we can also add queries to same association with different class name
+
+  has_many :products_available, lambda { where("inventory > 0") }, class_name: 'Product'
+
 
   validates :prefix, presence: true
   validates :name, presence: true
